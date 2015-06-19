4bd6e607c43ff9e238f24b6055795337e680a917
Anand.Kapadia <anand.kapadia1@gmail.com>
2015-06-19 13:50:19 -0400

    Interface to read .scanjsignore file added

--- scanner.js
+++ scanner.js
@@ -13,16 +13,18 @@ var ScanJS = require(__dirname + '/common/scan');
 
 var signatures = JSON.parse(fs.readFileSync(__dirname + "/common/rules.json", "utf8"));
 var createZip = false;
+var ignoreFile = '.scanjsignore';
+var ignoreContent = [];
 
 ScanJS.parser(parser);
 ScanJS.loadRules(signatures);
 
-var argv = require('optimist').usage('Usage: $node scan.js -t [path/to/app] -o [resultFile.json] --zip').demand(['t']).argv;
+var argv = require('optimist').usage('Usage: $node scan.js -t [path/to/app] -o [resultFile.json] --zip --ignore [ignorefilename]').demand(['t']).argv;
 
 var dive = function(dir, action) {
-  if( typeof action !== 'function') {
+  if (typeof action !== 'function') {
     action = function(error, file) {
-      console.log(">" + file)
+      console.log(">" + file);
     };
   }
   list = fs.readdirSync(dir);
@@ -30,10 +32,10 @@ var dive = function(dir, action) {
     var fullpath = dir + '/' + file;
     try {
       var stat = fs.statSync(fullpath);
-    } catch(e) {
+    } catch (e) {
       console.log("SKIPPING FILE: Could not stat " + fullpath);
     }
-    if(stat && stat.isDirectory()) {
+    if (stat && stat.isDirectory()) {
       dive(fullpath, action);
     } else {
       action(file, fullpath);
@@ -41,19 +43,19 @@ var dive = function(dir, action) {
   });
 };
 
-var directoryNameToFileLocation = function(filename) {
-  try{
-  filename = filename.replace(/\\|\//g, "=");
-  }
-  catch (e) {
-    console.log("Error with filename creation");
+var filePathToFileNameForZip = function(filename) {
+  try {
+    // enables integration of JSON results and the vulnerable ZIP in the Web GUI, linking filenames to fullpaths
+    filename = filename.replace(/\\|\//g, "="); 
+  } catch (e) {
+    console.log("Error: filepath could not be converted to a valid filename for the zip.");
   }
   return filename;
 };
 
 var writeReport = function(results, name) {
   if(fs.existsSync(name)) {
-    console.log("Error:output file already exists (" + name + "). Supply a different name using: -o [filename]")
+    console.log("Error:output file already exists (" + name + "). Supply a different name using: -o [filename]");
   }
   fs.writeFile(name, JSON.stringify(results), function(err) {
     if(err) {
@@ -64,8 +66,8 @@ var writeReport = function(results, name) {
   });
 };
 
-var downloadZip = function(zipfile, filename) {
-  var buffer = zipfile.generate({
+var downloadZip = function(zipFile, filename) { //camel
+  var buffer = zipFile.generate({
     type: "nodebuffer"
   });
   fs.writeFile(filename, buffer, function(err) {
@@ -77,16 +79,78 @@ var downloadZip = function(zipfile, filename) {
   });
 };
 
+var readIgnoreFile = function() {
+  if (!fs.existsSync(ignoreFile)) {
+    console.log("Error: Invalid Ignore File. Supply a valid ignore file using --ignore [ignoreFileName]");
+    return;
+  }
+  ignoreContent = fs.readFileSync(ignoreFile, 'utf8').toString().split(/\r\n|\n/g).filter(function(e) {
+    return e !== '';
+  });;
+  console.log("ignoreAst: " + JSON.stringify(ignoreContent, null, 2));
+}
+
+var parseIgnoreFile = function(){
+  var i = 0;
+  while( i < ignoreContent.length ){
+    var stringToParse = ignoreContent[i];
+    if(stringToParse.substring(0,2) == './'){
+      stringToParse = "//" + stringToParse.substring(2);
+      ignoreContent[i] = stringToParse;
+      i++;
+    }
+    else if(stringToParse.substring(0,1) == '*'){
+      stringToParse = stringToParse.substring(1);
+      try{
+        stringToParse = stringToParse.substring(0, stringToParse.indexOf('*'));
+        ignoreContent[i] = stringToParse;
+        i++;
+      } catch(e){
+        console.log("Error: only one instance of * found in ignore file");
+        ignoreContent.splice(i,1);
+      }
+    }
+    else if(stringToParse.substring(0,1) == '/'){
+      ignoreContent[i] = stringToParse;
+      i++;
+    } else{
+      ignoreContent.splice(i,1);
+    } 
+  }
+  //DEBUG
+  console.log(JSON.stringify(ignoreContent, null, 2));
+}
+
+var ignoreThisPath = function(filepath) {
+  for (var i = 0; i < ignoreContent.length; i++) {
+    var index = filepath.indexOf(ignoreContent[i]);
+    if (index > -1) {
+      if(ignoreContent[i].substring(0,1) == '/'){
+        return index==0;
+      }
+      else {
+        //DEBUG:
+        console.log("filepath ignored: " + filepath);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 var zip = new JSZip();
 
-if( typeof process != 'undefined' && process.argv[2]) {
+if (typeof process != 'undefined' && process.argv[2]) {
   results = {};
   reportname = argv.o ? argv.o : 'scanresults';
   reportdir = reportname + "_files";
-  createZip = argv.zip ? argv.zip : false;
+  createZip = argv.zip;
+  ignoreFile = argv.ignore ? argv.ignore : '.scanjsignore';
+  readIgnoreFile();
+  parseIgnoreFile();
   if(fs.existsSync(reportname) || fs.existsSync(reportdir)) {
-    console.log("Error:output file or dir already exists (" + reportname + "). Supply a different name using: -o [filename]")
-  } 
+    console.log("Error:output file or dir already exists (" + reportname + "). Supply a different name using: -o [filename]");
+  }
   else {
     fs.mkdirSync(reportdir);
     dive(argv.t, function(file, fullpath) {
@@ -98,18 +162,24 @@ if( typeof process != 'undefined' && process.argv[2]) {
         var content = beautify(content, { indent_size: 2 });
 
         var ast = parser.parse(content, { locations: true });
+        // DEBUG
+        console.log("file to scan: " + fullpath);
+        if (!ignoreThisPath(fullpath)) {
+          var scanresult = ScanJS.scan(ast, fullpath);
 
-        var scanresult = ScanJS.scan(ast, fullpath);
-        if (scanresult.type == 'error') {
-          console.log("SKIPPING FILE: Error in "+ fullpath+", at Line "+ scanresult.error.loc.line +", Column "+scanresult.error.loc.column+ ": " + scanresult.error.message);
-        }
-        if (scanresult.length > 0) {
-          if (scanresult[0]["type"] == 'finding') {
-            var fileToZipName = directoryNameToFileLocation(scanresult.filename);
-            zip.file(fileToZipName, content);
+          if (scanresult.length > 0) {
+            if (scanresult[0]["type"] == 'error') {
+              console.log("SKIPPING FILE: Error in " + fullpath + ", at Line " + scanresult.error.loc.line + ", Column " + scanresult.error.loc.column + ": " + scanresult.error.message);
+            }
+            if (scanresult[0]["type"] == 'finding') {
+              var fileToZipName = filePathToFileNameForZip(scanresult.filename);
+              zip.file(fileToZipName, content);
+            }
           }
+          results[fullpath] = scanresult;
+        } else {
+          console.log("This file was ignored: " + fullpath);
         }
-        results[fullpath] = scanresult;
       }
     });
     // Flatten report file to remove files with no findings and tests with no results (i.e. empty arr)
@@ -130,5 +200,5 @@ if( typeof process != 'undefined' && process.argv[2]) {
     }
   }
 } else {
-  console.log('usage: $ node scan.js path/to/app ')
+  console.log('usage: $ node scan.js path/to/app ');
 }
